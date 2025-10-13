8a09e701f52a7fee71ca6aa04bd2aae1efbc6768
DiamondOsas <diamondosazuwa@gmail.com>
2025-10-13 19:41:46 +0100

    Vercel Fix 5

--- SpamBot/tailwind.config.js
+++ SpamBot/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: { extend: {}, },
-  plugins: [],
-}
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}", 
+            "./index.html",],
+   theme: {
+     extend: {},
+   },
+   plugins: [],
+ }
